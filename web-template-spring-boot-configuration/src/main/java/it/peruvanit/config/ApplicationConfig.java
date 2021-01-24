222cff536b6f863399ff31e0370e4cf3a108ff82
Sergio Arellano <sergioarellanodiaz@gmail.com>
2021-01-24 16:41:19 +0100

    Refactoring ApplicationConfig

--- web-template-spring-boot-configuration/src/main/java/it/peruvanit/config/ApplicationConfig.java
+++ web-template-spring-boot-configuration/src/main/java/it/peruvanit/config/ApplicationConfig.java
@@ -3,11 +3,15 @@ package it.peruvanit.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import lombok.Data;
 
 @Configuration
-@PropertySource(value="classpath:web-template-spring-boot.properties", ignoreResourceNotFound=false)
+@PropertySources({
+	@PropertySource(value = {"file:${catalina.home}/conf/web-template-spring-boot.properties"}, ignoreResourceNotFound = true),
+	@PropertySource(value="classpath:web-template-spring-boot.properties", ignoreResourceNotFound=true)
+})
 @ConfigurationProperties(prefix = "io.peruvianit")
 @Data
 public class ApplicationConfig{
